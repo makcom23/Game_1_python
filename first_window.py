0ef3345a69f6cd56241113c4c88795556b69ee37
makcom23 <dynatek.m@gmail.com>
2025-04-23 20:39:11 +0300

    Слова в квадратах

--- first_window.py
+++ first_window.py
@@ -8,13 +8,40 @@ class First_Window:
         self.rect_1 = (250, 100, 300, 50)
         self.rect_2 = (250, 200, 300, 50)
         self.rect_3 = (250, 300, 300, 50)
-
+        self.font = pygame.font.SysFont(None, 36)
+        self.color_font = (100, 150, 100)
+        self.is_active = False
 
     def rectangles(self):
-        pygame.draw.rect(self.surface, self.color, self.rect_1, width=1, border_radius=5)
-        font = pygame.font.SysFont(None, 36)
-        img = font.render('First player', True, (100, 150, 100))
-        self.surface.blit(img, self.rect_1)
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    self.is_active = True
+                else:
+                    self.is_active = False
+                    
 
-        pygame.draw.rect(self.surface, self.color, self.rect_2, width=1, border_radius=5)
-        pygame.draw.rect(self.surface, self.color, self.rect_3, width=1, border_radius=5)
+        if self.is_active:
+            width =5 
+        else: width=1
+
+        rect1 = pygame.Rect(self.rect_1)
+        pygame.draw.rect(self.surface, self.color, rect1, width, border_radius=5)
+        center1 = rect1.center
+        text1 = self.font.render('New player', True, self.color_font)
+        text1_center = text1.get_rect(center=center1)
+        self.surface.blit(text1, text1_center)
+
+        rect2 = pygame.Rect(self.rect_2)
+        pygame.draw.rect(self.surface, self.color, rect2, width, border_radius=5)
+        center2 = rect2.center
+        text2 = self.font.render('Start game', True, self.color_font)
+        text2_center = text2.get_rect(center=center2)
+        self.surface.blit(text2, text2_center)
+
+        rect3 = pygame.Rect(self.rect_3)
+        pygame.draw.rect(self.surface, self.color, rect3, width, border_radius=5)
+        center3 = rect3.center
+        text3 = self.font.render('Quit', True, self.color_font)
+        text3_center = text3.get_rect(center=center3)
+        self.surface.blit(text3, text3_center)

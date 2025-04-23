c64c306fdb5af5ed870878ec0730a8d294f7e891
ssalo <shdzsalo@gmail.com>
2025-04-23 17:34:52 +0300

    add button

--- Button.py
+++ Button.py
@@ -12,18 +12,20 @@ class Button:
         self.COLOR_TEXT = (255, 255, 255)
         self.font = pygame.font.Font(None, 36)
         self.text = text
-        self.text_surface = self.font.render(self.text, True, self.COLOR_TEXT)
-        self.text_rect = self.text_surface.get_rect(center=(self.surface.get_width() // 2, self.surface.get_height() // 2))
-        self.rect = pygame.Rect(self.text_rect.x - self.radius, self.text_rect.y - self.radius, self.text_rect.width + 2 * self.radius, self.text_rect.height + 2 * self.radius)
         self.width = width
         self.height = height
+        self.is_active = False
 
     def show_button(self, x, y):
-        self.rect.topleft = (x, y)
         pygame.draw.rect(self.surface, self.COLOR_BORDER, (x, y, self.width, self.height), width=1, border_radius=5)
         img = self.font.render('First player', True, self.COLOR_TEXT)
         text_rectangle = img.get_rect()
-        x = self.rect.x + (self.width - text_rectangle.width) // 2
-        y = self.rect.y + (self.height - text_rectangle.height) // 2
+        x = x + (self.width - text_rectangle.width) // 2
+        y = y + (self.height - text_rectangle.height) // 2
         self.surface.blit(img, (x, y))
+        if self.is_active:
+            pygame.draw.rect(self.surface, self.COLOR_SELECTED, (x, y, self.width, self.height), width=10, border_radius=5)
+        
+    def set_active(self, is_active):
+        self.is_active = is_active
         

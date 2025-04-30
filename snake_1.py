4d26b56975ef77c3f7edd8be534a49841be6c7e9
makcom23 <dynatek.m@gmail.com>
2025-05-01 00:34:38 +0300

    Добавил победу, если осталось 5% свободного пространства, очки удваиваются
    (для теста стоит 99%)

--- snake_1.py
+++ snake_1.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import random as rnd
+import json
 from abstract_snake import AbstractSnake
 
 class Snake(AbstractSnake):
@@ -10,6 +11,9 @@ class Snake(AbstractSnake):
         self.cell_size = cell_size
         self.half_cell = cell_size // 2
         self.tail_pos = []
+        self.window_width = window_width
+        self.window_height = window_height
+        self.win_sound = pygame.mixer.Sound ( "win.wav" )
        
 
     def show(self):
@@ -29,8 +33,13 @@ class Snake(AbstractSnake):
         del self.tail_pos[self.tail_count+1:]
 
     def game_over(self):
+        total_cells = self.window_width * (self.window_height-20) // self.cell_size**2
+        occupied_cells = len(self.tail_pos)
+        free_percent = ((total_cells - occupied_cells) / total_cells)*100  
         if self.pos in self.tail_pos[1:]:
-            self.finish()
+            self.finish() # finish loose
+        elif free_percent <= 99:
+            self.win() # finish win
    
     def check(self):
         if self.apple_was_eaten():
@@ -42,3 +51,35 @@ class Snake(AbstractSnake):
         elif (self.apl.x, self.apl.y) in self.tail_pos:
                 return True
         return False
+    
+    def win(self):
+        
+        if self.settings.get_setting('sound'):
+            pygame.mixer.Sound.play(self.win_sound) # sound win
+        text = pygame.font.SysFont('Orbitron', 50)
+        img = text.render('YOU WIN!!!', True, self.COLOR_STATUSBAR)
+        self.surface.blit(img, (250, 250))
+        pygame.display.update() 
+        pygame.time.delay(5000)
+
+        # save game result #####################
+        player_name = self.settings.get_setting('name')
+        score = self.settings.get_setting('score')
+        score = score * 2
+        self.settings.set_setting('score', score)
+        with open('results.json') as res:
+            results = json.load(res)
+        if score > results[player_name]: # check player result with exist results
+            results[player_name] = score
+    
+            # sorting results.json
+            sorted_results = dict(sorted(results.items(), key=lambda item: item[1], reverse=True)) 
+        
+            with open('results.json', 'w', encoding='utf-8') as res:
+                json.dump(sorted_results, res, indent=4)
+        #####################
+            
+            self.settings.set_setting('game_state', 0)
+        else:
+            # exit
+            self.settings.set_setting('game_state', 0)
